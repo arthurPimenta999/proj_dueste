b55e8e234fa88abc0d42e6f82f24a74103a781fd
rMoreira113 <rafacar9moreira@gmail.com>
2024-05-03 20:48:35 -0300

    adicionei funções CRUD, so falta integra-las

--- FIrebase/FirebaseConfig.ts
+++ FIrebase/FirebaseConfig.ts
@@ -22,14 +22,16 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-import { collection, addDoc, updateDoc, deleteField } from "firebase/firestore"; 
+import { collection, addDoc, deleteDoc, getDoc} from "firebase/firestore"; 
 
-
-let IDu, unome, email, tel, senha,unnome, nemail, ntel, nsenha;
+//algumas variaveis para usar posterioremnte ~~ Rafinha
 let pnome, ppreco, pimagem, pingredientes;
 let lnome, lpreco, limagem;
 
 
+//função de adicionar documentos ao firestore ~~ Rafinha
+//adicionar USUARIO:  ~~Rafinha
+async function writedata(unome: string, tel: number, email: string, senha: string): Promise<any> {
 try {
   const docRef = await addDoc(collection(db, "usuario"), {
     email: email,
@@ -42,19 +44,55 @@ try {
   console.error("Erro em adicionar documento:", e);
 }
 
+}
 
-
-try {
-    const docRef = await updateDoc(doc(db, "usuario", IDu), {
-      email: nemail,
-      nome: unnome,
-      telefone: ntel,
-      senha: nsenha
+async function updatedata(unome: string, tel: number, email: string, senha: string): Promise<any> {
+  try {
+    const docRef = await addDoc(collection(db, "usuario"), {
+      email: email,
+      nome: unome,
+      telefone: tel,
+      senha: senha
     });
     console.log("Documento registrado no ID: ", docRef.id);
   } catch (e) {
     console.error("Erro em adicionar documento:", e);
   }
   
+  }
 
 
+async function deletedata(collection: string, id: string) {
+    try {
+      const docRef = await deleteDoc(doc(db,collection, id));
+      console.log("Documento deletado com sucesso");
+    } catch (e) {
+      console.error("Erro ao deletar documento do ID:", id)
+    }
+}
+
+async function readata(collection: string, id: string,) {
+
+  const docRef = doc(db, collection, id);
+  const docSnap = await getDoc(docRef)
+
+  try {
+      docSnap;
+  } catch (e) {
+    console.error("Erro ao encontrar documento do ID:", id)
+  }
+
+  if (docSnap.exists()) {
+    console.log("Documento do ID:" + id + ":", docSnap.data());
+  }else {
+    console.log("nenhum documento encontrado!");
+  }
+}
+
+const dataoperations = {
+  deletedata,
+  writedata,
+  readata
+}
+
+export default dataoperations;
